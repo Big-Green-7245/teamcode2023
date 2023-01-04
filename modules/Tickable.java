6947f755d3cf00da909e730972f93e9581b4339b
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-01-04 13:24:37 -0500

    Added tick documentation

--- modules/Tickable.java
+++ modules/Tickable.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+/**
+ * See <a href="https://minecraft.fandom.com/wiki/Tick">Tick</a>
+ */
 public interface Tickable {
     /**
      * Called after initialization is complete and before start is pressed.
